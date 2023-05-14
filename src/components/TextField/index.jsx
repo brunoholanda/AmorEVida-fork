9613c590551b05e54ed96419da137d8e55e33039
Bruno Holanda <holanda_rodrigues@hotmail.com>
2023-05-14 11:30:57 -0300

    Add files via upload

---
+++ src/components/TextField/index.jsx
@@ -0,0 +1,15 @@
+// const TextField = ({ type, placeholder, value, onChange }) => {
+//   return (
+//     <div>
+//         <input
+//             value={value}
+//             onChange={onChange}
+//             type={type}
+//             placeholder={placeholder}
+//         >
+//         </input>
+//     </div>
+//   )
+// }
+
+// export default TextField
